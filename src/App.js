ffb22ea723533cdcf9aac12b216a625b4ec620d9
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-06 16:16:42 -0800

    updated app with react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
-    return <PortfolioContainer />;
-}
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import NoMatch from "./pages/NoMatch";
+
+const App = () => {
+    document.title = "NM-Portfolio";
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route exact path={["/", "/about"]}>
+                        <About />
+                    </Route>
+                    <Route exact path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route exact path="/portfolio">
+                        <Portfolio />
+                    </Route>
+                    <Route>
+                        <NoMatch />
+                    </Route>
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
 
 export default App;
