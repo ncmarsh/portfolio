079a72e565b75126d507c3550d1f85399d80bdca
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-06 17:36:52 -0800

    removed unnecessary code

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "../components/Grid";
-// import Container from "../components/Container";
-// import Row from "../components/Row";
-// import Col from "../components/Col";
 
 function NoMatch() {
     return (
