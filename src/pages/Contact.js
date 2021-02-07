c53a1b4a4370a232cd79d4e61a3abf6d4a404848
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-06 16:11:07 -0800

    added contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Row, Col } from "../components/Grid";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
+
+function Contact() {
+    return(
+        <Container>
+            <Row>
+                <Col size="md-8">
+                    <h1>Contact</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Contact;
