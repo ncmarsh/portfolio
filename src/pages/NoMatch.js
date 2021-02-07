a575b3d473ea636379fd429e11d24500ac363721
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-07 13:04:19 -0800

    updated 404 page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "../components/Grid";
+import ContentCard from "../components/ContentCard";
 
 function NoMatch() {
     return (
         <Container>
             <Row>
-                <Col size="md-12">
-                    <h1>404 Page Not Found</h1>
-                </Col>
+                <ContentCard>
+                    <Col size="md-12">
+                        <h1>404 Page Not Found</h1>
+                    </Col>
+                </ContentCard>
             </Row>
         </Container>
     )
