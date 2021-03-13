3039d8e2f920078e96072b715617a230ddaa18d4
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-03-12 23:19:11 -0800

    updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "../components/Grid";
 import ContentCard from "../components/ContentCard";
-import ContactInfo from "../components/ContactInfo";
 
 const styles = {
     myPic: {
@@ -22,15 +21,25 @@ function About() {
                         <hr />
                         <img className="img-fluid" style={styles.myPic} src={process.env.PUBLIC_URL + "/assets/headshot.jpg"} alt="headshot" />
                         <p>
-                            Hello and welcome to my page! My name is Nicole Marshall and I am a Front-End Developer who has earned a certificate in Full-Stack Web Development from the University of Washington Coding Boot Camp in February 2021. I enjoy building intuitive, responsive and interactive web applications, both on my own and through collaboration with a team. Among the coding languages I have learned so far, I have found that I enjoy using JavaScript the most because of its versatility of creating both interactive and static elements and its usability between front-end and back-end development.
+                            Hello and welcome! My name is Nicole Marshall and I am a Full-Stack Developer who has earned a certificate in Full-Stack Web Development from the University of Washington Coding Boot Camp in February 2021. I enjoy building intuitive, responsive and interactive web applications, both on my own and through collaboration with a team. Among the coding languages I have learned so far, I have found that I enjoy using JavaScript the most because of its versatility of creating both interactive and static elements and its usability between front-end and back-end development.
                         </p>
                         <p>
                             Outside of coding, my hobbies include playing video games and board games, jigsaw puzzles, dancing and when it's nice out, hiking (though what I consider a hike, others refer to as nature walks). Back in the day, I also frequented the movie theater and enjoyed travelling... remember that?
                         </p>
                         <Row>
                             <Col size="xs-6 md-8">
-                                <h3>Connect with Me</h3>
-                                <ContactInfo />
+                                <h4>Skills</h4>
+                                <ul>
+                                    <li>React</li>
+                                    <li>JavaScript</li>
+                                    <li>MySQL</li>
+                                    <li>NoSQL</li>
+                                    <li>Node</li>
+                                    <li>Express</li>
+                                    <li>MERN Stack</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                </ul>
                             </Col>
                         </Row>
                     </Col>
